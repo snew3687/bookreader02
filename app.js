cf53a2c51e67f2ef833c3429e7209dd5308fb794
snew3687 <snew@hvccc.com.au>
2016-06-07 21:14:41 +1000

    Simplified express app setup
    1. Use express variable for root directory
    2. Commented/remove expressjs setup thought not needed
    3. Added applocal.js to make app locally-executable

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ var app = express();
 var bookServer = require('./bookServer.js');
 bookServer.initialiseServer(
   {
-    booksDocRoot: '..\\public\\content\\books'
+    booksDocRoot: path.join(__dirname, 'public', 'content', 'books')
+	//__dirname + '\\public\\content\\books'
   }
 );
 
 
-/**** Commented out express default routes to pick up bookreader routes
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**** Commented out express default routes to pick up bookreader routes
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,43 +30,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 ******/
 
@@ -105,6 +69,38 @@ app.get('/books/:bookUri/chapter/:chapterNumber', function(request, response) {
     .send(chapterContent);
 });
 
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
---
+++ applocal.js
@@ -0,0 +1,5 @@
+var app = require('./app.js');
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);

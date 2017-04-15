75f04ccaa699f6bf840e928d2d4192f4f7e9d0c3
Stephen New <stephenjnew.contact@gmail.com>
2017-04-15 22:22:41 +1000

    Changed server to allow CORS, for development.

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.use(cookieParser());
 app.use(logger('dev'));
 
 // parse application/x-www-form-urlencoded
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
